6718e2e514a9850912e87c7a143ba6aff8f82d25
--add <cycarrenol@unal.edu.co>
2019-07-01 23:02:32 -0500

    push de project

---
+++ tictactoe/src/index.js
@@ -0,0 +1,9 @@
+
+/* eslint-env browser */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game';
+import './index.css';
+
+ReactDOM.render(<Game />, document.getElementById('root'));
